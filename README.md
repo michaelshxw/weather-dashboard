c00bd63acdf88950354d97990ce61c90fbec980b
michaelshxw <michaelshxw@gmail.com>
2021-04-26 14:41:18 +0800

    update readme.md

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-empty
+# Weather Dashboard
+
+## Completed by Michael Shaw 
+## UWA Coding Bootcamp: Week 6 Homework Assignment
+
+This Weather Dashboard was created for a homework assignment for UWA's coding bootcamp.
+
+This application makes use of JavaScript to display current weather and a 5-day forecast for the selected city. 
+
+![](./screenshots/landing-page.png)
+
+### Getting Started
+
+This project has been deployed to GitHub Pages. To use the weather dashboard, you can follow the deployment link. Or, download the source files to use this as a template.
+
+* [Deployed GitHub IO](https://michaelshxw.github.io/weather-dashboard/)
+* [GitHub Repository](https://github.com/michaelshxw/weather-dashboard)
+
+### Prerequisites
+To install this application, a text editor such as Visual Studio Code is required. This can be downloaded [here.](https://code.visualstudio.com/download)
+
+### Languages Used
+* HTML 
+* CSS
+* JavaScript
+
+### Installation
+To install this code, download the zip file, or use GitHub's guidelines to clone the repository
+
+### Summary
+* This weather dashboard uses HTML, CSS and JavaScript to display the current and future weather forecast for a selected city.
+* This project highlights the use of JavaScript to make dynamic changes to a HTML document.
+
+![](./screenshots/landing-page.png)
+
+### Project Features
+* A Search Area
+    * A user can enter their city and click the search button to display data
+
+![](./screenshots/search.png)
+
+* A current weather output section
+    * This area is where the current weather for the selected city will be shown.
+
+![](./screenshots/current-weather.png)
+
+* A 5-day weather forecast
+    * This area is where the 5-day weather forecast for the selected city will be shown.
+
+![](./screenshots/forecast.png)
+    
+### Project Requirements 
+
+Below is the acceptance criteria that was given for the homework assignment
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+### Authors
+* **Michael Shaw** (HTML, CSS and JavaScript) [GitHub Profile](https://github.com/michaelshxw)
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,6 @@ var index = 0;
 searchBtn.click(function () {
     var searchContent = $(".searchContent").val();
     urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchContent + "&Appid=" + apiKey + "&units=metric";
-    console.log(urlCurrent);
     if (searchContent == "") {
         alert("Please enter a city")
     } else {
@@ -26,42 +25,34 @@ searchBtn.click(function () {
             localStorage.setItem(index, httpResponse.name);
             //add a iteration to each index so it doesn't overwrite
             index = index + 1;
-
-
             //get current city and date, display 
             var today = new Date();
+            //correct the month
             var month = today.getMonth() + 1
             var date = today.getDate() + "-" + month + "-" + today.getFullYear();
             var currentDate = $(".currentDate")
             currentDate.empty();
             currentDate.append(httpResponse.name + " " + date)
-
-
             //get icon and display
             var icon = $(".icon");
             icon.empty();
             icon.append(`<img src="https://openweathermap.org/img/wn/${httpResponse.weather[0].icon}@2x.png">`);
-
             //get current temp of city, display 
             var currentTemp = $(".currentTemp");
             currentTemp.empty();
             currentTemp.append("Temperature: " + httpResponse.main.temp.toFixed(0) + "°C")
-
             //get current wind of city, display
             var currentWind = $(".currentWind");
             currentWind.empty();
             currentWind.append("Wind speed: " + httpResponse.wind.speed.toFixed(1) + "km/h")
-
             //get current humidity in city, display
             var currentHumidity = $(".currentHumidity");
             currentHumidity.empty();
             currentHumidity.append("Humidity: " + httpResponse.main.humidity + "%")
             var lat = httpResponse.coord.lat;
             var lon = httpResponse.coord.lon;
-            
-
+            //get uv index, display
             var uvIndexURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=` + apiKey
-
             $.ajax({
                 url: uvIndexURL,
                 method: "GET"
@@ -69,22 +60,29 @@ searchBtn.click(function () {
                 var currentUV = $(".uvIndex");
                 currentUV.empty();
                 currentUV.append("UV Index: " + httpResponse.value)
-
             })
-            })
-        };
+            //five day forecast
+            var fiveDayForecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchContent + "&appid=" + apiKey + "&units=metric"
+            $.ajax({
+                url: fiveDayForecastURL,
+                method: "GET"
+            }).then(function (httpResponse) {
+                //to ensure you get the 12pm time for weather
+                var day = [2, 10, 18, 26, 34];
+                var forecastDiv = $(".forecast");
+                forecastDiv.empty();
+                //for each of the 5 days in the forecast, do this...
+                day.forEach(function (i) {
+                    //get the local time
+                    var time = new Date(httpResponse.list[i].dt * 1000);
+                    //convert the time into a readable format (australia) so the date is the right way around
+                    time = time.toLocaleDateString("en-AU");
+                    //append the following items to the forecastDiv card (date, icon, temp, wind, humidity)
+                    forecastDiv.append('<div class="col">' + "<br></br>" + "<p>" + time + "</p>" + `<img src="https://openweathermap.org/img/wn/${httpResponse.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + httpResponse.list[i].main.temp.toFixed(0) + "°C" + "</p>" + "<p>" + "Wind speed: " + httpResponse.list[i].wind.speed.toFixed(1) + "km/h" + "</p>" + "<p>" + "Humidity: " + httpResponse.list[i].main.humidity + "%" + "</p>" + "</div>");
+                })
 
-    });
-
-//displayCurrentWeather
-
-// displayFiveDayForecast
-
-    //get next date, in order from one day after current date, display
-    //get icon and display
-    //get temp for day, display
-    //get wind for day, display
-    //get humidity for day, display
-
-
+            });
+        });
+    };
+});
 
